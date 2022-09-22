1106003dc4db83ed82a8a30f341a6f5a3f8cc8d6
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2022-09-22 11:10:27 +0200

    funkční, ale nic moc design

---
+++ src/cz/chesters/stopky/Cibuloid.java
@@ -0,0 +1,105 @@
+package cz.chesters.stopky;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Cibuloid extends JPanel implements ActionListener {
+
+    JLabel display = new JLabel("Select an image to display");
+    File[] obrazky = new File("./imgs/").listFiles();
+    File[] thumbnaily = new File("./imgs/thumbnails").listFiles();
+    JButton test = new JButton(new ImageIcon("imgs/thumbnails/IMG_7456.JPG"));
+    JButton bigPic = new JButton();
+    ArrayList<JButton> menuBtns = new ArrayList<>();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public Cibuloid(){
+        test.setBounds(370, 530, 60, 45);
+        test.setBorder(new LineBorder(new Color(53, 54, 58), 1));
+        test.setForeground(Color.RED);
+        test.setFocusPainted(true);
+        test.setContentAreaFilled(false);
+        test.setMargin(new Insets(0, 0, 0, 0));
+        // prepare(test);
+
+        bigPic.setBounds(0, 0, 800, 600);
+        bigPic.setBorder(null);
+        bigPic.setFocusPainted(true);
+        bigPic.setContentAreaFilled(false);
+        bigPic.setMargin(new Insets(0, 0, 0, 0));
+        prepare(bigPic, 2);
+        bigPic.addActionListener(this::clickityClickIsThatADick);
+
+        menuSetup();
+    }
+
+    public void menuSetup() {
+        int heightPos = 530;
+        int widthPos;
+        if (obrazky.length % 2 == 1) {
+            widthPos = ((obrazky.length - 1) / 2) * 60 + (((obrazky.length - 1) / 2) - 1) * 10 + 30;
+        } else {
+            widthPos = (obrazky.length / 2) * 60 + ((obrazky.length / 2) - 1) * 10 + 5;
+        }
+        if (widthPos < 20)
+            widthPos = 20;
+
+        for (int i = 0; i < obrazky.length; i++) {
+            menuBtns.add(new JButton(new ImageIcon(thumbnaily[i].getPath())));
+            menuBtns.get(i).setBounds(widthPos, heightPos, 60, 45);
+            widthPos = widthPos + 60 + 10;
+            menuBtns.get(i).addActionListener(this::clickityClickIsThatADick);
+            prepare(menuBtns.get(i));
+            menuBtns.get(i).setBorder(new LineBorder(new Color(53, 54, 58), 1));
+            menuBtns.get(i).setFocusPainted(true);
+            menuBtns.get(i).setContentAreaFilled(false);
+            menuBtns.get(i).setMargin(new Insets(0, 0, 0, 0));
+            if (widthPos >= 580)
+                break;
+        }
+    }
+
+    public void clickityClickIsThatADick(ActionEvent e) {
+        if (e.getSource().equals(bigPic)){
+            System.out.println("BigClick");
+            bigPic.setIcon(null);
+        }
+        for (int i = 0; i < menuBtns.size(); i++) {
+            if (menuBtns.get(i).equals(e.getSource())) {
+                BufferedImage img;
+                try {
+                    img = ImageIO.read(new File(obrazky[i].getPath()));
+                    bigPic.setIcon(new ImageIcon(img.getScaledInstance(800,600, Image.SCALE_DEFAULT)));
+                } catch (IOException ee) {
+                    ee.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void prepare(JComponent o) {
+        o.setVisible(true);
+        this.add(o, Integer.valueOf(1));
+    }
+
+    public void prepare(JComponent o, int z) {
+        o.setVisible(true);
+        this.add(o, Integer.valueOf(z));
+    }
+
+    public void prepare(JComponent o, Integer z) {
+        o.setVisible(true);
+        this.add(o, z);
+    }
+}
--- src/cz/chesters/stopky/Galerie.java
+++ src/cz/chesters/stopky/Galerie.java
@@ -1,22 +1,12 @@
 package cz.chesters.stopky;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.io.File;
-import java.util.ArrayList;
 
 public class Galerie {
-    JFrame frame = new JFrame("Je čas měřit čas");
+    JFrame frame = new JFrame("Galerie");
 
-    JLayeredPane onion = new JLayeredPane();
-    ArrayList<JButton> menuBtns = new ArrayList<>();
-    JLabel display = new JLabel("Select an image to display");
-    File[] obrazky = new File("./imgs/").listFiles();
-    File[] thumbnaily = new File("./imgs/thumbnails").listFiles();
-    JButton test = new JButton(new ImageIcon("imgs/thumbnails/IMG_7456.JPG"));
-    JLabel bigPic = new JLabel();
+    Cibuloid onion = new Cibuloid();
+
 
     public void doShit() {
         galerijuj();
@@ -26,75 +16,16 @@ public class Galerie {
         frame.setSize(800, 638); // 4:3 plus horní lišta na windowsech
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        frame.setContentPane(n);
+        frame.setContentPane(onion);
         frame.setResizable(false);
 
-        onion.setPreferredSize(new Dimension(800,600));
-        frame.add(onion);
+        onion.setBounds(0,0,800,600);
         onion.setVisible(true);
-
-        test.setBounds(370, 530, 60, 45);
-        test.setBorder(new LineBorder(new Color(53, 54, 58), 1));
-        test.setForeground(Color.RED);
-        test.setFocusPainted(true);
-        test.setContentAreaFilled(false);
-        test.setMargin(new Insets(0, 0, 0, 0));
-        // prepare(test);
-
-        bigPic.setBounds(0,0,800,600);
-        prepare(bigPic, 0);
-
-        menuSetup();
 
         frame.setVisible(true);
     }
 
-    public void menuSetup() {
-        int heightPos = 530;
-        int widthPos = 0;
-        if (obrazky.length % 2 == 1) {
-            widthPos = ((obrazky.length - 1) / 2) * 60 + (((obrazky.length - 1) / 2) - 1) * 10 + 30;
-        } else {
-            widthPos = (obrazky.length / 2) * 60 + ((obrazky.length / 2) - 1) * 10 + 5;
-        }
-        if (widthPos < 20)
-            widthPos = 20;
 
-        for (int i = 0; i < obrazky.length; i++) {
-            menuBtns.add(new JButton(new ImageIcon(thumbnaily[i].getPath())));
-            menuBtns.get(i).setBounds(widthPos, heightPos, 60, 45);
-            widthPos = widthPos + 60 + 10;
-            menuBtns.get(i).addActionListener(this::clickityClickIsThatADick);
-            prepare(menuBtns.get(i));
-            if (widthPos >= 580)
-                break;
-        }
-    }
 
-    public void clickityClickIsThatADick(ActionEvent e) {
-        Object source = e.getSource();
-        for (int i = 0; i < menuBtns.size(); i++) {
-            if (menuBtns.get(i).equals(e.getSource())){
-                bigPic.setIcon(new ImageIcon(obrazky[i].getPath()));
-            }
-        }
-
-        frame.update(frame.getGraphics());
-    }
-
-    public void prepare(JComponent o) {
-        o.setVisible(true);
-        onion.add(o, Integer.valueOf(1));
-    }
-
-    public void prepare(JComponent o, int z) {
-        o.setVisible(true);
-        onion.add(o, Integer.valueOf(z));
-    }
-
-    public void prepare(JComponent o, Integer z) {
-        o.setVisible(true);
-        onion.add(o, z);
-    }
 
 }

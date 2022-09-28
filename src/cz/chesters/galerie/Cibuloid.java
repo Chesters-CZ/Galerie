960e43cdbab60b76d22fd8c0abe5d1c8602ded04
Chesters-CZ <62833819+Chesters-CZ@users.noreply.github.com>
2022-09-28 23:05:06 +0200

    omg i fucking did it

--- src/cz/chesters/galerie/Cibuloid.java
+++ src/cz/chesters/galerie/Cibuloid.java
@@ -14,8 +14,6 @@ import java.util.Objects;
 
 public class Cibuloid extends JPanel implements ActionListener {
 
-    JLabel display = new JLabel("Select an image to display");
-
     File[] obrazky = new File("./imgs/").listFiles();
     // MAKE SURE ./imgs/ IS CLEAN. THERE MUST BE NO NON-IMAGE FILES OR FOLDERS IN THERE
 
@@ -27,8 +25,8 @@ public class Cibuloid extends JPanel implements ActionListener {
     JLabel settings = new JLabel("Settings");
     JLabel gallery = new JLabel("Gallery");
     JLabel resolution = new JLabel("Resolution");
-    JComboBox<String> borderSelection = new JComboBox<>(new String[]{"Default", "None", "Normal", "Thick"});
-    JLabel borderStyle = new JLabel("Border style");
+    JComboBox<String> iconSelection = new JComboBox<>(new String[]{"Default", "Tiny", "Smol", "Humongous"});
+    JLabel iconSize = new JLabel("Icon size");
     Galerie parent;
 
     JToolBar menu = new JToolBar();
@@ -57,12 +55,12 @@ public class Cibuloid extends JPanel implements ActionListener {
         box.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
         bar.add(box);
 
-        borderSelection.setEditable(false);
-        borderSelection.setBounds(120, 115, 635, 30);
-        borderSelection.setVisible(true);
-        borderSelection.addActionListener(this::clickityTwoElectricBoogaloo);
-        borderSelection.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
-        bar.add(borderSelection);
+        iconSelection.setEditable(false);
+        iconSelection.setBounds(120, 115, 635, 30);
+        iconSelection.setVisible(true);
+        iconSelection.addActionListener(this::clickityTwoElectricBoogaloo);
+        iconSelection.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
+        bar.add(iconSelection);
 
         gallery.setBounds(2, 2, 796, 30);
         gallery.setVisible(true);
@@ -81,10 +79,10 @@ public class Cibuloid extends JPanel implements ActionListener {
         resolution.setHorizontalAlignment(SwingConstants.LEFT);
         bar.add(resolution);
 
-        borderStyle.setBounds(25, 115, 100, 30);
-        borderStyle.setVisible(true);
-        borderStyle.setHorizontalAlignment(SwingConstants.LEFT);
-        bar.add(borderStyle);
+        iconSize.setBounds(25, 115, 100, 30);
+        iconSize.setVisible(true);
+        iconSize.setHorizontalAlignment(SwingConstants.LEFT);
+        bar.add(iconSize);
 
         bar.setBounds(0, 0, 800, 200);
         bar.setFloatable(false);
@@ -98,93 +96,50 @@ public class Cibuloid extends JPanel implements ActionListener {
     }
 
     public void menuSetup() {
-        goGoGadget(530, 780, new LineBorder(new Color(53, 54, 58), 1));
+        goGoGadget(530, 780, new Rectangle(60, 45));
     }
 
-    public void menuSetup(boolean isBig, LineBorder border) {
+    public void menuSetup(boolean isBig, Rectangle size) {
         if (isBig)
-            goGoGadget(530, 780, border);
-        else goGoGadget(370, 620, border);
+            goGoGadget(530, 780, size);
+        else goGoGadget(370, 620, size);
     }
 
-    public void scratchThat(boolean isBig, LineBorder border) {
-        if (isBig)
-            bigPic.setBounds(0, 0, 800, 600);
-        else bigPic.setBounds(0, 0, 640, 480);
-
-        bigPic.setBorder(null);
-        bigPic.setFocusPainted(true);
-        bigPic.setContentAreaFilled(false);
-        bigPic.setMargin(new Insets(0, 0, 0, 0));
-        prepare(bigPic, 2);
-        bigPic.addActionListener(this::clickityClickIsThatADick);
-        bigPic.setVisible(false);
-
-        box.setEditable(false);
-        box.setBounds(120, 75, 635, 30);
-        box.setVisible(true);
-        box.addActionListener(this::clickityTwoElectricBoogaloo);
-        box.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
-        bar.add(box);
-
-        borderSelection.setEditable(false);
-        borderSelection.setBounds(120, 115, 635, 30);
-        borderSelection.setVisible(true);
-        borderSelection.addActionListener(this::clickityTwoElectricBoogaloo);
-        borderSelection.setFont(new Font(Font.DIALOG, Font.BOLD, 14));
-        bar.add(borderSelection);
-
-        settings.setBounds(2, 2, 796, 25);
-        settings.setVisible(true);
-        settings.setFont(new Font(Font.DIALOG, Font.BOLD, 20));
-        settings.setHorizontalAlignment(SwingConstants.CENTER);
-        bar.add(settings);
-
-        resolution.setBounds(25, 75, 100, 30);
-        resolution.setVisible(true);
-        resolution.setHorizontalAlignment(SwingConstants.LEFT);
-        bar.add(resolution);
-
-        borderStyle.setBounds(25, 115, 100, 30);
-        borderStyle.setVisible(true);
-        borderStyle.setHorizontalAlignment(SwingConstants.LEFT);
-        bar.add(borderStyle);
-
-        bar.setBounds(0, 0, 800, 200);
-        bar.setFloatable(false);
-        bar.setVisible(true);
-        bar.setLayout(null);
-        add(bar);
-
-        this.setLayout(null);
-
-        menuSetup(isBig, border);
-    }
-
-    public void goGoGadget(int yPos, int maxX, LineBorder border) {
+    public void goGoGadget(int yPos, int maxX, Rectangle size) {
+        for (JButton menuBtn : menuBtns) {
+            menu.remove(menuBtn);
+            menuBtn.removeNotify();
+        }
+        menuBtns = new ArrayList<>();
         menu.setBounds(0, 200, maxX + 20, yPos + 55);
         menu.setFloatable(false);
         menu.setVisible(true);
         menu.setLayout(new FlowLayout());
         int widthPos = 0;
+
+        if (obrazky.length > 50) {
+            System.out.println("You are trying to display a lot of pictures. Please wait, while the program loads them.");
+            System.out.println("Should you get an OutOfMemory Exception, try reducing the number of images and their size.");
+        }
+
         for (int i = 0; i < obrazky.length; i++) {
             try {
-                menuBtns.add(new JButton(new ImageIcon(ImageIO.read(new File(obrazky[i].getPath())).getScaledInstance(60, 45, Image.SCALE_DEFAULT))));
+                menuBtns.add(new JButton(new ImageIcon(ImageIO.read(new File(obrazky[i].getPath())).getScaledInstance(size.width, size.height, Image.SCALE_DEFAULT))));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            menuBtns.get(i).setBounds(widthPos, yPos, 60, 45);
-            widthPos = widthPos + 60 + 10;
+            menuBtns.get(i).setBounds(widthPos, yPos, size.width, size.height);
+            widthPos = widthPos + size.width + 10;
             menuBtns.get(i).addActionListener(this::clickityClickIsThatADick);
             menuBtns.get(i).setVisible(true);
-            menuBtns.get(i).setBorder(border);
             menuBtns.get(i).setFocusPainted(true);
             menuBtns.get(i).setContentAreaFilled(false);
-            menuBtns.get(i).setMargin(new Insets(5, 5, 5, 5));
+            menuBtns.get(i).setMargin(new Insets(0, 0, 0, 0));
+            menuBtns.get(i).setBorder(new LineBorder(new Color(0, 0, 0), 0));
             menu.add(menuBtns.get(i));
             widthPos++;
         }
-                prepare(menu);
+        prepare(menu);
     }
 
     public void clickityClickIsThatADick(ActionEvent e) {
@@ -198,7 +153,7 @@ public class Cibuloid extends JPanel implements ActionListener {
                 BufferedImage img;
                 try {
                     img = ImageIO.read(new File(obrazky[i].getPath()));
-                    bigPic.setIcon(new ImageIcon(img.getScaledInstance(800, 600, Image.SCALE_DEFAULT)));
+                    bigPic.setIcon(new ImageIcon(img.getScaledInstance(bigPic.getWidth(), bigPic.getHeight(), Image.SCALE_DEFAULT)));
                     bigPic.setVisible(true);
                 } catch (IOException ee) {
                     ee.printStackTrace();
@@ -209,7 +164,7 @@ public class Cibuloid extends JPanel implements ActionListener {
 
     private void clickityTwoElectricBoogaloo(ActionEvent e) {
         boolean isBig = true;
-        LineBorder border = null;
+        Rectangle size = null;
         if (Objects.equals(box.getSelectedItem(), "800x600")) {
             parent.frame.setSize(800, 638);
             try {
@@ -221,7 +176,8 @@ public class Cibuloid extends JPanel implements ActionListener {
             gallery.setBounds(2, 2, 796, 25);
             box.setBounds(120, 75, 635, 30);
             bar.setLayout(null);
-            borderSelection.setBounds(120, 115, 635, 30);
+            iconSelection.setBounds(120, 115, 635, 30);
+            bigPic.setBounds(0, 0, 800, 600);
         }
         if (Objects.equals(box.getSelectedItem(), "640x480")) {
             isBig = false;
@@ -235,22 +191,23 @@ public class Cibuloid extends JPanel implements ActionListener {
             gallery.setBounds(2, 2, 636, 25);
             box.setBounds(120, 75, 425, 30);
             bar.setLayout(null);
-            borderSelection.setBounds(120, 115, 425, 30);
+            iconSelection.setBounds(120, 115, 425, 30);
+            bigPic.setBounds(0, 0, 640, 480);
         }
 
-        if (Objects.equals(borderSelection.getSelectedItem(), "Default")) {
-            border = new LineBorder(new Color(53, 54, 58), 1);
-        } else if (Objects.equals(borderSelection.getSelectedItem(), "None")) {
-            border = new LineBorder(new Color(53, 54, 58), 0);
-        } else if (Objects.equals(borderSelection.getSelectedItem(), "Normal")) {
-            border = new LineBorder(new Color(53, 54, 58), 2);
-        } else if (Objects.equals(borderSelection.getSelectedItem(), "Thick")) {
-            border = new LineBorder(new Color(53, 54, 58), 5);
+        if (Objects.equals(iconSelection.getSelectedItem(), "Default")) {
+            size = new Rectangle(60, 45);
+        } else if (Objects.equals(iconSelection.getSelectedItem(), "Tiny")) {
+            size = new Rectangle(16, 12);
+        } else if (Objects.equals(iconSelection.getSelectedItem(), "Smol")) {
+            size = new Rectangle(32, 24);
+        } else if (Objects.equals(iconSelection.getSelectedItem(), "Humongous")) {
+            size = new Rectangle(100, 75);
         }
 
-        menuSetup(isBig, border);
-
+        menuSetup(isBig, size);
         parent.frame.repaint();
+        parent.frame.revalidate();
     }
 
     public void prepare(JComponent o) {
